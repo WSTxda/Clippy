4d50e7eea65cfcb89eafbf20af1ccf49e7fefe39
WSTxda <wstxda@gmail.com>
2024-10-30 02:03:38 -0300

    Improvement ShortenerRemover regex

--- app/src/main/java/com/wstxda/clippy/tracker/utils/ShortenerRemover.kt
+++ app/src/main/java/com/wstxda/clippy/tracker/utils/ShortenerRemover.kt
@@ -13,7 +13,20 @@ object ShortenerRemover {
         Regex("(clid|irclickid|click_id|clickid)", RegexOption.IGNORE_CASE),
         Regex("utm_\\w+", RegexOption.IGNORE_CASE),
         Regex("ref", RegexOption.IGNORE_CASE),
-        Regex("si", RegexOption.IGNORE_CASE)
+        Regex("si", RegexOption.IGNORE_CASE),
+        Regex("campaign[_-]?id", RegexOption.IGNORE_CASE),
+        Regex("promo[_-]?code", RegexOption.IGNORE_CASE),
+        Regex("ad_id", RegexOption.IGNORE_CASE),
+        Regex("ad_group_id", RegexOption.IGNORE_CASE),
+        Regex("fbclid", RegexOption.IGNORE_CASE),
+        Regex("gclid", RegexOption.IGNORE_CASE),
+        Regex("msclkid", RegexOption.IGNORE_CASE),
+        Regex("referrer", RegexOption.IGNORE_CASE),
+        Regex("session_id", RegexOption.IGNORE_CASE),
+        Regex("visitor_id", RegexOption.IGNORE_CASE),
+        Regex("user_id", RegexOption.IGNORE_CASE),
+        Regex("click_id", RegexOption.IGNORE_CASE),
+        Regex("tracking_id", RegexOption.IGNORE_CASE)
     )
 
     fun removeShortenerParamsFromUrl(
@@ -31,7 +44,11 @@ object ShortenerRemover {
         }
 
         val cleanUri = cleanUriBuilder.build().toString()
-        return if (uri.fragment != null) "$cleanUri#${uri.fragment}" else cleanUri
+        return if (uri.fragment != null) {
+            "$cleanUri#${uri.fragment}"
+        } else {
+            cleanUri
+        }
     }
 
     private fun isShortenerParameter(param: String): Boolean {
