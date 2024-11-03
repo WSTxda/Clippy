486c206d2220b5fc55475042536e587a93cff284
WSTxda <wstxda@gmail.com>
2024-11-03 01:25:25 -0300

    Remove texts shared/copied along with links before copy to clipboard
    Now remove additional text (usually from the marketplace) to just copy the link to the clipboard.
    
    Example:
    
    > Look at this incredible product I found: https:/market/product
    
    To:
    
    > https:/market/product

--- app/src/main/java/com/wstxda/clippy/activity/ClipboardLinkActivity.kt
+++ app/src/main/java/com/wstxda/clippy/activity/ClipboardLinkActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
 import com.wstxda.clippy.R
+import com.wstxda.clippy.cleaner.tools.TextCleaner
 
 abstract class ClipboardLinkActivity : AppCompatActivity() {
 
@@ -15,11 +16,9 @@ abstract class ClipboardLinkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val action = intent.action
-        val sharedLink = when {
-            action == Intent.ACTION_PROCESS_TEXT -> intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT)
-                ?.trim()
-
-            action == Intent.ACTION_SEND -> intent.getStringExtra(Intent.EXTRA_TEXT)?.trim()
+        val sharedLink = when (action) {
+            Intent.ACTION_PROCESS_TEXT -> intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT)?.trim()
+            Intent.ACTION_SEND -> intent.getStringExtra(Intent.EXTRA_TEXT)?.trim()
                 ?: intent.getStringExtra(Intent.EXTRA_SUBJECT)?.trim()
 
             else -> null
@@ -29,8 +28,10 @@ abstract class ClipboardLinkActivity : AppCompatActivity() {
     }
 
     private fun handleLink(sharedLink: String?) {
-        if (!sharedLink.isNullOrEmpty()) {
-            processLink(sharedLink)
+        val extractedLink = sharedLink?.let { TextCleaner.extractUrl(it) }
+
+        if (!extractedLink.isNullOrEmpty()) {
+            processLink(extractedLink)
         } else {
             showToast(getString(R.string.copy_failure))
             finishActivity()
