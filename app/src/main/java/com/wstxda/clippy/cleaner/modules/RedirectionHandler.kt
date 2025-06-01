24aec0d21e5caa902d587e2ec69b417accb34118
WSTxda <wstxda@gmail.com>
2025-06-01 18:23:48 -0300

    Make RedirectionHandler resolve multiples redirect links to get final url correctly

--- app/src/main/java/com/wstxda/clippy/cleaner/modules/RedirectionHandler.kt
+++ app/src/main/java/com/wstxda/clippy/cleaner/modules/RedirectionHandler.kt
@@ -6,26 +6,48 @@ import java.net.URL
 
 object RedirectionHandler {
     private const val DEFAULT_TIMEOUT_MILLIS = 3000
+    private const val MAX_REDIRECTS = 3
+    private const val TAG = "RedirectionHandler"
 
-    fun resolveRedirectionParams(url: String): String {
-        var connection: HttpURLConnection? = null
-        try {
-            connection = (URL(url).openConnection() as HttpURLConnection).apply {
-                instanceFollowRedirects = false
-                connectTimeout = DEFAULT_TIMEOUT_MILLIS
-                readTimeout = DEFAULT_TIMEOUT_MILLIS
-                connect()
+    fun resolveRedirectionParams(initialUrl: String): String {
+        var url = initialUrl
+        var redirects = 0
+
+        while (redirects < MAX_REDIRECTS) {
+            val result = runCatching {
+                (URL(url).openConnection() as HttpURLConnection).apply {
+                    instanceFollowRedirects = false
+                    connectTimeout = DEFAULT_TIMEOUT_MILLIS
+                    readTimeout = DEFAULT_TIMEOUT_MILLIS
+                    connect()
+                }.use { connection ->
+                    val responseCode = connection.responseCode
+                    if (responseCode in 300..399) {
+                        connection.getHeaderField("Location") ?: return url
+                    } else {
+                        return url
+                    }
+                }
             }
-            val responseCode = connection.responseCode
-            return when (responseCode) {
-                in 300..399 -> connection.getHeaderField("Location") ?: url
-                else -> url
-            }
-        } catch (e: Exception) {
-            Log.e("RedirectionHandler", "Error resolving URL: ${e.message}", e)
-            return url
+
+            result.fold(onSuccess = { redirectedUrl ->
+                if (redirectedUrl == url) return redirectedUrl
+                url = redirectedUrl
+                redirects++
+            }, onFailure = { e ->
+                Log.e(TAG, "Error resolving URL: ${e.message}", e)
+                return url
+            })
+        }
+
+        return url
+    }
+
+    private inline fun <T> HttpURLConnection.use(block: (HttpURLConnection) -> T): T {
+        return try {
+            block(this)
         } finally {
-            connection?.disconnect()
+            disconnect()
         }
     }
 }
